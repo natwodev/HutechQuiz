7bac3398b8e56e89ea963be1e2f7d9da85fb2718
Nguyễn Huỳnh Nam <nhuynhnamht2023@gmail.com>
2025-07-24 00:02:16 +0700

    cập nhật đơn phân công

--- backend_manage/backend_manage/Entities/ExamRoomLecturerAssignment.cs
+++ backend_manage/backend_manage/Entities/ExamRoomLecturerAssignment.cs
@@ -1,18 +1,21 @@
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace backend_manage.Entities
 {
     public class ExamRoomLecturerAssignment : BaseEntity
     {
+        // Khóa chính của bảng ExamSessionDepartment (tự động tăng)
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ExamRoomLecturerAssignmentId { get; set; }
         public int ExamRoomId { get; set; }
         public ExamRoom ExamRoom { get; set; }
-
+        
+        public int ExamSessionSubjectId { get; set; }
+        public ExamSessionSubject ExamSessionSubject { get; set; }
         public int LecturerId { get; set; }
         public Lecturer Lecturer { get; set; }
-
-        // Có thể thêm các trường khác nếu cần, ví dụ:
-        // public string AssignmentStatus { get; set; }
-        // public string Note { get; set; }
+        
     }
 } 
