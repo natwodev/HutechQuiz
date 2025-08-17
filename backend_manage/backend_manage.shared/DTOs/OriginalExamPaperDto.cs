b13681def25bb702a82a54d336bd93c05c3ba058
Nguyen Huynh Nam <nhuynhnamht2023@gmail.com>
2025-08-17 14:29:28 +0700

    cải thiện lại front end + back end(bug tạo đề hoán vị)

--- backend_manage/backend_manage.shared/DTOs/OriginalExamPaperDto.cs
+++ backend_manage/backend_manage.shared/DTOs/OriginalExamPaperDto.cs
@@ -41,4 +41,20 @@ namespace backend_manage.shared.DTOs
         public int DurationMinutes { get; set; }
         public int TotalQuestions { get; set; }
     }
+
+    public class OriginalExamPaperDetailShufferDto
+    {
+        public int OriginalExamPaperDetailId { get; set; }
+        public int Order { get; set; }
+        public string? QuestionContent { get; set; }
+        public string? Answer1 { get; set; }
+        public string? Answer2 { get; set; }
+        public string? Answer3 { get; set; }
+        public string? Answer4 { get; set; }
+        public int? CorrectAnswerIndex { get; set; }
+        public int? ParentQuestionId { get; set; }
+        public bool CanShuffleQuestion { get; set; }
+        public string? AnswerShuffleInfo { get; set; } 
+    }
+    
 } 
