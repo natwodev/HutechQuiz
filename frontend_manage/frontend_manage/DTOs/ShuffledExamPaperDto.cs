e0128278b49a3f9883287dd559c818c710e250be
Nguyen Huynh Nam <nhuynhnamht2023@gmail.com>
2025-08-21 20:14:50 +0700

    thiết kế lại front end exam

--- frontend_manage/frontend_manage/DTOs/ShuffledExamPaperDto.cs
+++ frontend_manage/frontend_manage/DTOs/ShuffledExamPaperDto.cs
@@ -12,26 +12,78 @@ public class ShuffledExamPaperDto
     public string SubjectName { get; set; }
     public string SubjectCode { get; set; }
     public int? ExamSessionSubjectId { get; set; }
-    public List<ShuffledExamPaperDetailDto> Details { get; set; }
+        
+    //public string? QuestionStructure { get; set; }
+        
+    public List<QuestionStructureDto> QuestionStructures { get; set; } = new();
+        
 }
 
-public class ShuffledExamPaperDetailDto
+public class QuestionStructureDto
 {
-    public int ShuffledExamPaperDetailId { get; set; }
-    public int Order { get; set; }
-    public string AnswerOrder { get; set; }
+
     public int OriginalExamPaperDetailId { get; set; }
+        
     public int? ParentQuestionId { get; set; }
-    public string QuestionContent { get; set; }
-    public string Answer1 { get; set; }
-    public string Answer2 { get; set; }
-    public string Answer3 { get; set; }
-    public string Answer4 { get; set; }
-    public List<ShuffledExamPaperDetailDto> ChildQuestions { get; set; } = new();
+        
+    public int Order { get; set; }
+    public string? QuestionContent { get; set; }
+    public List<QuestionStructureDto> ChildQuestions { get; set; } = new();
+        
+    public List<AnswerStructureDto> Answers { get; set; } = new();
+}
+    
+public class AnswerStructureDto
+{
+    public int AnswerId { get; set; }
+        
+    public int Order { get; set; }
+    public string AnswerContent { get; set; }
+        
+    public int OriginalExamPaperDetailId { get; set; }
+}
+
+public class OriginalExamPaperDto
+{
+    public int OriginalExamPaperId { get; set; }
+    public string OriginalExamPaperCore { get; set; }
+    public string Title { get; set; }
+    public string? Description { get; set; }
+    public int SubjectId { get; set; }
+    public int DurationMinutes { get; set; }
+    public int TotalQuestions { get; set; }
+    public string? KeyValueList { get; set; }
+    public List<OriginalExamPaperDetailDto> Details { get; set; } = new();
+}
+
+public class OriginalExamPaperDetailDto
+{
+    public int OriginalExamPaperDetailId { get; set; }
+    public int Order { get; set; }
+    public string? QuestionContent { get; set; }
+    public int? CorrectAnswerIndex { get; set; }
+    public int? ParentQuestionId { get; set; }
+    public int ChapterId { get; set; }
+    public bool CanShuffleQuestion { get; set; }
+    public string? AnswerShuffleInfo { get; set; }
+    public OriginalExamPaperDetailDto? ParentQuestion { get; set; }
+    public List<OriginalExamPaperDetailDto> ChildQuestions { get; set; } = new();
+    public List<AnswerDto> Answers { get; set; } = new();
+}
+
+public class AnswerDto
+{
+    public int AnswerId { get; set; }
+    public int Order { get; set; }
+    public string AnswerContent { get; set; }
+    public bool IsCorrect { get; set; }
+    public bool CanShuffleAnswer { get; set; }
+    public int OriginalExamPaperDetailId { get; set; }
 }
 
 public class StartExamResponseDto
 {
     public StudentExamSessionCacheDto StudentSession { get; set; }
     public ShuffledExamPaperDto ExamPaper { get; set; }
+    public OriginalExamPaperDto OriginalExamPaper { get; set; }
 } 
